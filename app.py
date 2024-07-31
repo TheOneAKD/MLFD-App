78039568d5e5bbb5b27c6e94584158a5902f1fc7
Bramblestar <95667168+TheOneAKD@users.noreply.github.com>
2024-07-31 12:34:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -592,6 +592,6 @@ def handle_session_ended():
 if __name__ == '__main__':
     env = os.environ.get('MY_APP_ENV', 'production')
     if env == 'production':
-        socketio.run(app, host='0.0.0.0', port=8000, allow_unsafe_werkzeug=True)
+        socketio.run(app, host='0.0.0.0', port=8000, debug=False, allow_unsafe_werkzeug=True)
     else:
         socketio.run(app, host='0.0.0.0', port=8000, debug=True)
