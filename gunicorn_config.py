308ed7674325b4d622636120553d10a5c7f46860
Bramblestar <95667168+TheOneAKD@users.noreply.github.com>
2024-07-31 13:00:35 -0400

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,22 +1,8 @@
 import multiprocessing
 
-# Determine the number of CPU cores
-workers = multiprocessing.cpu_count() * 2 + 1
+def calculate_workers():
+    return (multiprocessing.cpu_count() * 2) + 1
 
-# Use eventlet worker class
+workers = calculate_workers()
 worker_class = 'eventlet'
-
-# Specify the host and port
 bind = '0.0.0.0:8000'
-
-# Number of worker threads
-threads = 4
-
-# Set maximum number of requests a worker will process before restarting
-max_requests = 1000
-max_requests_jitter = 50
-
-# Logging configuration
-loglevel = 'info'
-accesslog = '-'
-errorlog = '-'
