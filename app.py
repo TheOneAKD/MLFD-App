9edb3b265cef75035b0b38e252011b3cc3ee7633
Bramblestar <95667168+TheOneAKD@users.noreply.github.com>
2024-07-31 13:01:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -591,4 +591,5 @@ def handle_session_ended():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)), debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
