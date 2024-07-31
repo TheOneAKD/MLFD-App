1236de47dc53dcf00a71eeeabeb0fe7e9599358d
Bramblestar <95667168+TheOneAKD@users.noreply.github.com>
2024-07-31 13:18:37 -0400

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,3 @@
 import multiprocessing
 
-def calculate_workers():
-    return (multiprocessing.cpu_count() * 2) + 1
-
-workers = calculate_workers()
-worker_class = 'eventlet'
-bind = '0.0.0.0:8000'
+workers = multiprocessing.cpu_count() * 2 + 1
